04bab2ed2a16811faaad7bea5b3effba4802ec56
Mathilde Arconte <signup@marconte.fr>
2021-03-10 17:14:43 +0100

    sectionsHome section method

--- templates/section-method.php
+++ templates/section-method.php
@@ -1,3 +1,10 @@
+<?php
+	$title = get_sub_field('title');
+	$items = get_sub_field('items');
+?>
 <section class="section section-methode">
-Hello
+<?php if ( $title ) : ?>
+	<h2><?php echo $title ?></h2>
+<?php endif; ?>
+
 </section>
