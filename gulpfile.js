5ccdcad7a3df539cc9473e52c13fe4033690dccb
Mathilde Arconte <signup@marconte.fr>
2021-03-10 17:28:23 +0100

    sectionsHome changes

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@
 /* SETTINGS
   /=  ================================================== =  = */
 // local domain used by browsersync
-var browsersync_proxy = "http://whs.local";
+var browsersync_proxy = "http://localhost:10028";
 
 // default asset paths
 var assets = {
--- templates/section-method.php
+++ templates/section-method.php
@@ -1,3 +1,21 @@
+<?php
+	$title = get_sub_field('title');
+?>
 <section class="section section-methode">
-Hello
+<?php if ( $title ) : ?>
+	<h2><?php echo $title ?></h2>
+<?php endif; ?>
+<?php if ( have_rows('items') ) : ?>
+
+	<?php while( have_rows('items') ) : the_row(); ?>
+		<h3><?php the_sub_field('title'); ?></h3>
+		<p><?php the_sub_field('text'); ?></p>
+		<?php if ( get_sub_field('img') ) : ?>
+			<img src="<?php the_sub_field('img'); ?>" alt="<?php the_sub_field('title'); ?>">
+		<?php endif; ?>
+
+	<?php endwhile; ?>
+
+<?php endif; ?>
+
 </section>
