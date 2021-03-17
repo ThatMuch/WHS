58f4fbc263259a44ed9f045d7d4cea264390d5c2
Mathilde Arconte <signup@marconte.fr>
2021-03-17 14:04:52 +0100

    custom post type

---
+++ calendar.php
@@ -0,0 +1,12 @@
+<?
+/**
+ * Template for ACF flexible elements
+ *
+ * @author      ThatMuch
+ * @version     0.1.0
+ * @since       mars_1.0.0
+ *
+ *
+ */
+?>
+<?php /* Template Name: Calendrier */ ?>
--- functions/functions-custom_whs.php
+++ functions/functions-custom_whs.php
@@ -51,4 +51,47 @@ function whs_acf_op_init() {
 }
 }
 
+function cptui_register_my_cpts_events() {
+
+	/**
+	 * Post Type: Rentrées.
+	 */
+
+	$labels = [
+		"name" => __( "Rentrées", "WHS" ),
+		"singular_name" => __( "Rentrée", "WHS" ),
+	];
+
+	$args = [
+		"label" => __( "Rentrées", "WHS" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => [ "slug" => "events", "with_front" => true ],
+		"query_var" => true,
+		"menu_icon" => "dashicons-calendar",
+		"supports" => [ "title" ],
+		"taxonomies" => [ "category" ],
+	];
+
+	register_post_type( "events", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts_events' );
+
+
+
 
