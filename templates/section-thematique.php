11cd1d8a4e74e015cfd22f329e3f9e85f8cb0eed
Mathilde Arconte <signup@marconte.fr>
2021-03-22 17:56:53 +0100

    entreprise section thematique

---
+++ templates/section-thematique.php
@@ -0,0 +1,22 @@
+<section class="section__area">
+	<div class="container">
+		<div class="row row-15">
+			<?php if ( have_rows( 'items' ) ) : ?>
+				<?php while ( have_rows( 'items' ) ) : the_row(); ?>
+					<div class="col-md-6 col-lg-4">
+						<div class="card-box bg__black">
+							<h2><?php the_sub_field( 'titre' ); ?></h2>
+							<?php $image = get_sub_field( 'image' ); ?>
+							<?php if ( $image ) : ?>
+							<div class="card-box__image">
+								<img src="<?php echo esc_url( $image['url'] ); ?>" alt="<?php echo esc_attr( $image['alt'] ); ?>" />
+							</div>
+							<?php endif; ?>
+							<p><?php the_sub_field( 'texte' ); ?></p>
+						</div>
+					</div>
+				<?php endwhile;?>
+			<?php endif;?>
+		</div>
+	</div>
+</section>
