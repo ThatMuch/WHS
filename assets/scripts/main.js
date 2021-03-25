3717b3c97778b0240bc1a46f582f6f56fe07131e
Mathilde Arconte <signup@marconte.fr>
2021-03-25 01:01:14 +0100

    fixe scroll up

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,7 +5,7 @@
 
         var headerHeight = $('.header-wrapper').height();
 
-        $('.scrollUp').click(function() {
+    /*     $('.scrollUp').click(function() {
             if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
                 var target = $(this.hash);
                 target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -16,7 +16,7 @@
                     return false;
                 }
             }
-        });
+        }); */
 
     });
 
