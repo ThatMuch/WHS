ba34f0ccc95a392f79038bc27218e4fff89c0dec
Mathilde Arconte <signup@marconte.fr>
2021-03-26 12:59:46 +0100

    privacy policy page

---
+++ templates/wp-policy.php
@@ -0,0 +1,23 @@
+<?
+/**
+ * @author      ThatMuch
+ * @version     0.1.0
+ * @since       mars_1.0.0
+ */
+?>
+
+<?php get_header(); ?>
+<main id="page" class="section__area bg__black line__black ">
+
+      <section class="container pt-5">
+    <?php while (have_posts()) : the_post(); ?>
+        <?php the_content(); ?>
+    <?php endwhile; ?>
+      </section>
+
+
+</main>
+
+
+
+<?php get_footer(); ?>
