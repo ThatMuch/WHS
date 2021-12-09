b7cebadbc1527664793a88157b82754fc37b9215
Mathilde Arconte <signup@marconte.fr>
2021-12-09 12:34:23 +0100

    remove link if is empty

--- templates/section-formidable.php
+++ templates/section-formidable.php
@@ -6,7 +6,9 @@
 						<?php if( get_sub_field('titre') ) : ?>
 							<div class="f-22"><?php echo get_sub_field('titre'); ?></div>
 						<?php endif; ?>
+                        <?php if( get_sub_field('lien') ) : ?>
                         <a href="<?php the_sub_field( 'lien' ); ?>" target="_blank" class="btn btn-yellow mt-40"><span>réserver un créneau avec le fondateur</span></a>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
